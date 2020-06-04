8a8bbf81dfadab854864999e66b22667a9f742fb
Ubuntu <ubuntu@ip-172-31-46-51.us-west-2.compute.internal>
2020-06-04 05:43:33 +0000

    AWS first run

--- train_vanilla.py
+++ train_vanilla.py
@@ -24,18 +24,18 @@ from DQN_agent import *
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Running on", device)
 
-CCHECKPOINT_FOLDERNAME = "checkpoints/"
-CHECKPOINT_PREFIX="vanilla_dqn"
-MEMORY_SIZE = 1e6 
+CHECKPOINT_FOLDERNAME = "checkpoints/"
+CHECKPOINT_PREFIX="vanilla_dqn_test" #####
+MEMORY_SIZE = 1e2 # 6 
 BURN_IN = 1e5
 LEARNING_RATE = 2.5e-4
 GAMMA = 0.99
-MAX_EPISODES = 5000
+MAX_EPISODES = 4 # 5000
 EPSILON = 0.25
 # INITIAL_EPSILON = 1; FINAL_EPSILON=0.1; FINAL_EPSILON_FRAME=1e6
 NETWORK_SYNC_FREQ = 10000
 NETWORK_UPDATE_FREQ = 4
-NETWORK_SAVE_FREQ = 100
+NETWORK_SAVE_FREQ = 2 # 100
 NETWORK_EVALUATE_FREQ = 100
 N_VAL_EPISODES = 10
 
@@ -52,7 +52,6 @@ agent = DQN_agent(env, dqn, D, epsilon=EPSILON)
 agent.train(
     gamma=GAMMA,
     max_episodes=MAX_EPISODES,
-    batch_size=BATCH_SIZE,
     network_update_frequency = NETWORK_UPDATE_FREQ,
     network_sync_frequency = NETWORK_SYNC_FREQ,
     network_save_frequency = NETWORK_SAVE_FREQ,
